4da2ab350cb9291e30ee355acd835b79749a8f81
shivam45783 <shivamrajan01@gmail.com>
2025-01-18 12:39:36 +0530

    added delete button

--- src/App.jsx
+++ src/App.jsx
@@ -51,10 +51,16 @@ function App() {
             </div>
             <div className="relative w-full h-0.5">
               <button
-                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5 hover:bg-blue-700"
+                className="absolute left-1/4 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5 hover:bg-blue-700"
                 onClick={() => swap()} // or we can write directly {swap}
               >
                 Swap
+              </button>
+              <button
+                className="absolute left-2/3 -translate-x-1/4 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5 hover:bg-blue-700"
+                onClick={() => setAmount(0)} // or we can write directly {swap}
+              >
+                Delete
               </button>
             </div>
             <div className="w-full mb-1 flex flex-col justify-center align-middle gap-2">
